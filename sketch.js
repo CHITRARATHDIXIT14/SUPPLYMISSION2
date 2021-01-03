b6881759543ec4429cc5a0c2488b1e0fd524ae8b
CHITRARATHDIXIT14 <76897412+CHITRARATHDIXIT14@users.noreply.github.com>
2021-01-03 14:00:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var box1,box2,box3
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	engine = Engine.create();
+	world = engine.world;
+
+   fill("red")
+	box3=createSprite(290,610,20,120)
+	box3.shapeColor=color("red")
+	box2=createSprite(360,650,150,20)
+	box2.shapeColor=color("red")
+    box1 =createSprite(440,610,20,120)
+	box1.shapeColor=color("red")
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+   
+
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	
+
+    var packageSprite_options = {
+
+		restitution:0.6
+
+
+
+
+
+	}
+	packageBody = Bodies.circle(width/2 , 200 , 5 , packageSprite_options);
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+ 
+	 box2 = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, box2);
+
+
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+  
+  
+  drawSprites();
+
+
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+    
+  }
+}
+
+
+
